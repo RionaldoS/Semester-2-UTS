25df7660a9c1125d07635bdf300e90934f98235f
RionaldoS <312400018@polbis.ac.id>
2025-04-20 12:01:33 +0700

    first commit untuk membuat Folder dan File menggunakan Bush

---
+++ EditFolder/PushGithub.sh
@@ -0,0 +1,11 @@
+
+git config --global user.email "312400018@polbis.ac.id"
+git config --global user.name "RionaldoS"
+
+echo "# Semester-2-UTS" >> README.md
+git init
+git add README.md
+git commit -m "first commit untuk membuat Folder dan File menggunakan Bush"
+git branch -M main
+git remote add origin https://github.com/RionaldoS/Semester-2-UTS.git
+git push -u origin main
